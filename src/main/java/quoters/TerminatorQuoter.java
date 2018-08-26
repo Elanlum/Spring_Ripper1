81cfae9b55b5d8c71617133e791e6e2c7f38a326
Roman Gorbatenko <roman_gorbatenko@epam.com>
2018-08-27 00:21:17 +0300

    PostProxy

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,11 +1,18 @@
 package quoters;
 
+import javax.annotation.PostConstruct;
+@Profiling
 public class TerminatorQuoter implements Quoter {
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
 
+    public void setRepeat(int repeat) {
+        this.repeat = repeat;
+    }
+
     private String message;
 
+    @PostConstruct
     public void init() {
         System.out.println("Phase 2");
         System.out.println(repeat);
@@ -20,7 +27,9 @@ public class TerminatorQuoter implements Quoter {
     }
 
     @Override
+    @PostProxy
     public void sayQuote() {
+        System.out.println("Phase 3");
         for (int i = 0; i < repeat; i++) {
             System.out.println("message = " + message);
         }
